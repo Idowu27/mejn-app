cb46644e7a4b25cc89b43d5bbc51004352b42d92
naomi <13243549+Idowu27@users.noreply.github.com>
2017-09-06 16:31:29 -0500

    set up Mongodb

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+/* this variable allows us to use the databse when we handle requests from the browser*/
+var db
+
+ MongoClient.connect('mongodb://idowu27:blues27@ds123534.mlab.com:23534/star-wars-quotez', (err, database) => {
+   if (err) return console.log(err)
+   db = database
+   app.listen(3000, () => {
+     console.log('listening on 3000')
+   })
+ })
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+/*replace function with => after the parameters*/
+
+app.get('/', (req, res) => {
+  /*the sendFile has index served back to the browser instead */
+    res.sendFile(__dirname + '/index.html')
+  })
+
+  app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
+
+
 console.log('May node be with you')
